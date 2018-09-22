f6bf0dedc5290ac1d26311a022133cdfd970a632
chirag jain <chirag8966@gmail.com>
2018-09-22 17:49:11 +0530

    sign in working
    check login_activity and place the required intent to be loaded when authentication successfull.

--- app/src/main/java/com/example/amanlahariya/blooddonation/AccountActivity/Login_Activity.java
+++ app/src/main/java/com/example/amanlahariya/blooddonation/AccountActivity/Login_Activity.java
@@ -1,22 +1,32 @@
 package com.example.amanlahariya.blooddonation.AccountActivity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import com.example.amanlahariya.blooddonation.MainActivity;
 import com.example.amanlahariya.blooddonation.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Login_Activity extends AppCompatActivity {
 
     private EditText etMobileView;
     private EditText etPasswordView;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,7 @@ public class Login_Activity extends AppCompatActivity {
         Button login = (Button) findViewById(R.id.bt_Sing_In);
         Button forgot = (Button) findViewById(R.id.bt_Forgot_Password);
         Button register = (Button) findViewById(R.id.bt_Register);
+        mAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,14 +57,13 @@ public class Login_Activity extends AppCompatActivity {
         forgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent_forgot = new Intent(Login_Activity.this, forgot_password .class);
+                Intent intent_forgot = new Intent(Login_Activity.this, forgot_password.class);
                 startActivity(intent_forgot);
             }
         });
     }
 
-    protected void validate(String user, String pass)
-    {
+    protected void validate(String user, String pass) {
         boolean cancel = false;
         View focusView = null;
 
@@ -76,37 +86,28 @@ public class Login_Activity extends AppCompatActivity {
         }
 
         else {
-            // perform the user login attempt.
-           /* if(user.equals("12345") && pass.equals("12345")) {*/
-                Intent intent_dash = new Intent(Login_Activity.this,MainActivity.class);
-                startActivity(intent_dash);
-            /*}
-            else
-            {
-                Toast toast = Toast.makeText(getApplicationContext(),"Incorrect username or password!",Toast.LENGTH_LONG);
-                toast.show();
-            /*}
-        }
-    }*/
+            mAuth.signInWithEmailAndPassword(user, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        private static final String TAG = "MainActivity";
 
-    mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-        @Override
-        public void onComplete(@NonNull Task<AuthResult> task) {
-            if (task.isSuccessful()) {
-                // Sign in success, update UI with the signed-in user's information
-                Log.d(TAG, "signInWithEmail:success");
-                FirebaseUser user = mAuth.getCurrentUser();
-                updateUI(user);
-            } else {
-                // If sign in fails, display a message to the user.
-                Log.w(TAG, "signInWithEmail:failure", task.getException());
-                Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show();
-                updateUI(null);
-            }
-
-            // ...
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //updateUI(user);
+                                Intent intent_sign = new Intent(Login_Activity.this, HomeFragment.class);
+                                startActivity(intent_sign);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(Login_Activity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
         }
-    });
+    }
 }
